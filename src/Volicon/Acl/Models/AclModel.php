a264d3413a32167e533351337c5e9a49a3919061
unknown <nadav.v@VINIK-PC.volicon.local>
2014-11-16 06:30:26 +0200

    remove AclModel::disableACL(),unguardAcl(),reguardAcl(),isUsingAcl()

--- src/Volicon/Acl/Models/AclModel.php
+++ src/Volicon/Acl/Models/AclModel.php
@@ -6,6 +6,7 @@ use Volicon\Acl\Permission;
 // use Volicon\Acl\Facades\Acl;
 use Acl;
 use App;
+use Auth;
 
 /**
  * Description of Model
@@ -13,14 +14,9 @@ use App;
  * @author nadav.v
  */
 class AclModel extends Model {
-	private $use_acl = true;
 	protected $acl_field_key = '';
-	private static $enable_acl = true;
+	
 	public function __construct($attributes = []) {
-		if (App::runningInConsole ()) {
-			$this->use_acl = FALSE;
-			self::$enable_acl = FALSE;
-		}
 		
 		parent::__construct ( $attributes );
 		
@@ -28,21 +24,11 @@ class AclModel extends Model {
 			$this->acl_field_key = $this->getKeyName ();
 		}
 	}
-	public static function disableACL() {
-		$self = new static ();
-		$self->use_acl = false;
-		return $self;
-	}
-	public static function unguardAcl() {
-		self::$enable_acl = FALSE;
-	}
-	public static function reguardAcl() {
-		self::$enable_acl = TRUE;
-	}
+	
 	public function newQuery() {
 		$builder = parent::newQuery ();
 		
-		if (! $this->isUsingAcl ()) {
+		if (!Acl::isGuard()) {
 			return $builder;
 		}
 		
@@ -108,12 +94,10 @@ class AclModel extends Model {
 	}
 	protected static function checkDeletingPermissions($model) {
 	}
-	final public function isUsingAcl() {
-		return $this->use_acl && self::$enable_acl;
-	}
+	
 	private static function _registerCreatingPermissions() {
 		static::creating ( function ($model) {
-			if ($model->isUsingAcl ()) {
+			if (Acl::isGuard()) {
 				$class = get_class ( $model );
 				$result = Acl::check ( $class . '.insert' );
 				if (! $result) {
@@ -123,7 +107,7 @@ class AclModel extends Model {
 			}
 		} );
 		static::creating ( function ($model) {
-			if ($model->isUsingAcl ()) {
+			if (Acl::isGuard()) {
 				$result = $this->checkCreatingPermissions ( $model );
 				if (! $result) {
 					throw new NoPermissionsException ( "No Permission to create $class" );
@@ -134,7 +118,7 @@ class AclModel extends Model {
 	}
 	private static function _registerUpdatingPermissions() {
 		static::updating ( function ($model) {
-			if ($model->isUsingAcl ()) {
+			if (Acl::isGuard()) {
 				$class = get_class ( $model );
 				$id = $model [$model->getAclKey ()];
 				$result = Acl::check ( $class . '.update', [ 
@@ -147,7 +131,7 @@ class AclModel extends Model {
 			}
 		} );
 		static::updating ( function ($model) {
-			if ($model->isUsingAcl ()) {
+			if (Acl::isGuard()) {
 				$result = $this->checkCreatingPermissions ( $model );
 				if (! $result) {
 					throw new NoPermissionsException ( "No Permission to update $class id:" . $id );
@@ -158,7 +142,7 @@ class AclModel extends Model {
 	}
 	private static function _registerDeletingPermissions() {
 		static::deleting ( function ($model) {
-			if ($model->isUsingAcl ()) {
+			if (Acl::isGuard()) {
 				$class = get_class ( $model );
 				$id = $model [$model->getAclKey ()];
 				$result = Acl::check ( $class . '.delete', [ 
@@ -171,7 +155,7 @@ class AclModel extends Model {
 			}
 		} );
 		static::deleting ( function ($model) {
-			if ($model->isUsingAcl ()) {
+			if (Acl::isGuard()) {
 				$result = $this->checkDeletingPermissions ( $model );
 				if (! $result) {
 					throw new NoPermissionsException ( "No Permission to delete $class id:" . $id );
