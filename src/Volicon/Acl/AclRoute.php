9c1be5ee56f4d7144ce56ac85cad6996f8091734
Nadav Vinik <email@nadavvin.com>
2014-11-19 12:13:55 +0200

    rename AclRoute::check() to AclRoute::validateRoute()

--- src/Volicon/Acl/AclRoute.php
+++ src/Volicon/Acl/AclRoute.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
  */
 class AclRoute {
 	
-	public static function check() {
+	public static function validateRoute() {
 		$route = Route::current();
 		
 		$route_name = $route->getName();
